a9d38a40286a1d2dfc55abef2823999254c5a8e7
Gabriel Santo <gvgesanto2@gmail.com>
2020-06-28 20:01:39 -0300

    creating the routes and controllers methods for create, read, update and delete bootcamps

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
+// Route files
+const bootcampRoutes = require("./routes/bootcamp.routes");
+
+// Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcampRoutes);
 
 const PORT = process.env.PORT || 5000;
 
