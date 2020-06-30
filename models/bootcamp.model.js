9f1c226e709c9cdab48be813b0865c38ae7bc4f5
Gabriel Santo <gvgesanto2@gmail.com>
2020-06-29 23:40:28 -0300

    created a mongoose middleware to add a slug property to the bootcamp object from its name property

--- models/bootcamp.model.js
+++ models/bootcamp.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -94,6 +95,11 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
