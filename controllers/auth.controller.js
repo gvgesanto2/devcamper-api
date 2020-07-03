0fc1cbeebafb50ced993e2a82d8bdb199883ed57
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-03 15:01:21 -0300

    created the user model and the register user functionality

---
+++ controllers/auth.controller.js
@@ -0,0 +1,20 @@
+const ErrorResponse = require('../utils/ErrorResponse');
+const asyncHandler = require('../middleware/asyncHandler');
+const User = require('../models/user.model');
+
+// @desc Register user
+// @route POST /api/v1/auth/register
+// @access Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  res.status(200).json({ success: true });
+});
