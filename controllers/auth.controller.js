5ed59b3ae13144e8b5cc802dcefabee331ddc849
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-03 23:07:34 -0300

    adding the forgot password functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -62,6 +62,27 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc Forgot password
+// @route POST /api/v1/auth/forgotpassword
+// @access Private
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset password
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
 const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth.controller');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword
+} = require('../controllers/auth.controller');
 
 const { protect } = require('../middleware/auth');
 
@@ -8,5 +13,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
