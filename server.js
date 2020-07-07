7f733a0e7aea6e9a71d9fedb8586c775c4b61eb6
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-07 14:21:49 -0300

    adding security headers and xss protection

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const bodyParser = require('body-parser');
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/db');
@@ -41,6 +43,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
